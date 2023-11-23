370a875370075c07e7aa4ee6e2fd1fc736bf44bf
daviu <daviu@DESKTOP-0BEA9Q2>
2023-11-23 19:40:55 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace EstruturaComposta
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int horas;
+
+            Console.WriteLine("Quantas horas?  ");
+
+            horas = int.Parse(Console.ReadLine());
+
+            //objetivo perguntar ao usuario que horas são e definir se vai ser bom dia ou boa tarde.
+
+            //Utilizando o if - se e else - se não, na estrutura composta.
+
+            if (horas < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else
+
+                if (horas < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
